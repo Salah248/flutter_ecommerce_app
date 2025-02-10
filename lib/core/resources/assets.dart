bc218dad7f103be23c6dfd74dbdc226349fd44d5
Salah248 <salahsadoon248@gmail.com>
2025-02-10 14:42:51 +0200

    set up the project and add splash screan

---
+++ lib/core/resources/assets.dart
@@ -0,0 +1,3 @@
+class ImageAsset{
+  static const String splash = 'assets/image/splash.png';
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_ecommerce_app/presentaion/splash/bloc/cubit/splash_cubit.dart';
+import 'package:flutter_ecommerce_app/presentaion/splash/splash.dart';
+
+void main() {
+  runApp(const OurMarket());
+}
+
+class OurMarket extends StatelessWidget {
+  const OurMarket({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => SplashCubit()..appStarted(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Our Market',
+        home: SplashPage(),
+      ),
+    );
+  }
+}
